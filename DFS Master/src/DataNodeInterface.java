4a6b6fbe5658b60e591d2bc635d79e4626d70dbd
Kaustubh Olpadkar <kaustubh.olpadkar@gmail.com>
2018-04-08 19:08:39 +0530

    Eclipse project and updated files

---
+++ DFS Master/src/DataNodeInterface.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface DataNodeInterface extends Remote{
+	public boolean push (String filename, Fragment file) throws RemoteException;
+	
+	public Fragment pull (String filename) throws RemoteException;
+	
+	public String helloServer() throws RemoteException;
+}
